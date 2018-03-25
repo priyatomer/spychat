97eea6ec49af470ad7ef1db39cb73b786ed66894
priya tomer <aqcifo@gmail.com>
2018-03-25 12:21:51 +0530

    spychat project

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 print "Hello Buddy!"
 print "Let\'s get started"
-spy_name = raw_input("What is your spy name? ")
-print "Welcome " + spy_name + ". Glad to have you back with us."
-spy_salutaion = raw_input("What should we call you (Mr. or Ms.)? ")
-spy_name = spy_salutaion + " " + spy_name
-print "Alright Ms. Priya. I'd like to know a little bit more about.."
+spy_name = raw_input("What is your spy name? ")#take name as input from user
+if spy_name.isspace():#tocheck for space input
+    print "Enter a valid name"
+if len(spy_name)>2:#checking for length of the string name
+    print "Welcome " + spy_name + ". Glad to have you back with us."#concatenating strings
+    spy_salutaion = raw_input("What should we call you (Mr. or Ms.)? ")
+    if spy_salutaion=="Mr." or spy_salutaion=="Ms.":#using if to check condition to take input of salutation
+        spy_name = spy_salutaion + " " + spy_name
+        print "Alright " + spy_name + ". I'd like to know a little bit more about you"
+        spy_age = input("What is your age?")
+        if 60>spy_age>14:#nested if statement to check range of age
+            print "Your age is correct."
+            spy_ratings = input("What is your ratings?")
+            if spy_ratings>8.0:
+                print "Great spy"
+            elif 5.5<spy_ratings<=8.0:#elif statement for more than one condition
+                print "Average spy"
+            elif 3.0<spy_ratings<=5.5:
+                print "Bad spy"
+            else:
+                print "Are you really a spy?"
+            spy_is_online = True
+            print "Authentication is completed. Welcome " + spy_name + "age:" + str(spy_age) + " and ratings: " + str(spy_ratings)
+        else:
+            print "Your age is not eligible to be a spy"
+     else:
+         print "Invalid salutation"
+else:
+    print "Ooopss Please enter a valid name"
